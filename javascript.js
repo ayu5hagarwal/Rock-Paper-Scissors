aeb062f998486b147e7ffd9a75821c6912fbe198
warndarkahead <ayush.it.21023@recb.ac.in>
2023-09-13 10:36:34 +0530

    counting is not working now

---
+++ javascript.js
@@ -0,0 +1,64 @@
+function getComputerChoice(){
+    const stringArray = ['rock','paper','scissors'];
+    const randomIndex = Math.floor(Math.random() * stringArray.length);
+    const randomlySelectedString = stringArray[randomIndex];
+    return randomlySelectedString; 
+}
+
+var pointsOfPlayer = 0;
+var pointsOfComputer = 0;
+
+ 
+function game(){
+   
+    function playRound(playerSelection, computerSelection) {
+        
+        let playerSelectionCaseInsensitive = playerSelection.toLowerCase();
+        if((playerSelectionCaseInsensitive == 'rock' && computerSelection == 'paper') || (playerSelectionCaseInsensitive == 'paper' && computerSelection == 'scissors') ||
+            (playerSelectionCaseInsensitive == 'scissors' && computerSelection == 'rock')){
+                pointsOfComputer++;
+                return ("You Lose! " + computerSelection + " beats " +  playerSelectionCaseInsensitive);
+               
+                
+            }
+        else if((playerSelectionCaseInsensitive == 'paper' && computerSelection == 'rock') || (playerSelectionCaseInsensitive == 'scissors' && computerSelection == 'paper') ||
+        (playerSelectionCaseInsensitive == 'rock' && computerSelection == 'scissors')){
+            pointsOfPlayer++;
+            return ("You Win! " + playerSelectionCaseInsensitive + " beats " + computerSelection);
+                
+        }
+        else if((playerSelectionCaseInsensitive == 'paper' && computerSelection == 'paper') || (playerSelectionCaseInsensitive == 'scissors' && computerSelection == 'scissors') ||
+        (playerSelectionCaseInsensitive == 'rock' && computerSelection == 'rock')){
+            return ("Tie!");
+        }
+        } 
+      const playerSelection = prompt();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+}
+
+
+for (let i = 1; i <= 5.; i++) {
+    game();    
+
+}
+
+if(pointsOfComputer>pointsOfPlayer){
+    console.log("Computer Wins!")
+}
+else if(pointsOfComputer<pointsOfPlayer){
+    console.log("You Wins!")
+}
+else if(pointsOfComputer=pointsOfPlayer){
+    console.log("Tie!")
+}
+    
+
+
+
+
+
+
+
+
+
